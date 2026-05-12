361cd6d5a9916906dac4229fbd70bf22397c2322
Kacper Wojtaszczyk <kacper.wojtaszczyk95@gmail.com>
2026-05-12 20:54:17 +0200

    add GH link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import '../app.css';
+	import GitHubLink from '$lib/components/GitHubLink.svelte';
 
 	let { children } = $props();
 </script>
+
+<GitHubLink />
 
 <main>
 	{@render children()}
